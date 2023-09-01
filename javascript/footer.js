557fcce64a82e5728482b20a77ebb399d520d0d7
Kervy Yu <kerbs.yu@gmail.com>
2023-09-01 18:27:28 +0800

    added gallery with changes in design

---
+++ javascript/footer.js
@@ -0,0 +1,47 @@
+document.write('\
+<link rel="stylesheet" href="/css/footer.css" />\
+<footer>\
+      <div class="container p-4">\
+        <div class="row">\
+          <div class="description col-lg-4 col-md-12 mb-4">\
+            <a class="mb-3">\
+              <a class="navbar-brand" href="mainpage.html"><img src="../images/fleurlogo.png" alt=""/></a>\
+            <p>\
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis\
+              molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam\
+              voluptatem veniam, est atque cumque eum delectus sint!\
+            </p>\
+          </div>\
+          <div class="links col-lg-4 col-md-6 mb-4">\
+            <h5 class="mb-3">links</h5>\
+            <ul class="list-unstyled  mb-0">\
+              <li class="mb-2">\
+                <a href="#hero">Home</a>\
+              </li>\
+              <li class="mb-2">\
+                <a href="#">Gallery</a>\
+              </li>\
+              <li class="mb-2">\
+                <a href="#">About</a>\
+              </li>\
+              <li>\
+                <a href="#" >Contact</a>\
+              </li>\
+            </ul>\
+          </div>\
+          <div class="short col-lg-4 col-md-6 mb-4">\
+            <h5 class="mb-3">Contact Us</h5>\
+              <p class="contact-icon"><i class="fas fa-home mr-3"></i> Bayswater, Western Australia</p>\
+              <p class="contact-icon"><i class="fas fa-envelope mr-3"></i> fleurloca@outlook.com</p>\
+              <p class="contact-icon"><i class="fas fa-phone mr-3"></i> +61 423 987 241</p>\
+              <div class="social-icon mt-4">\
+                <!-- Facebook -->\
+                 <a type="button" href="https://www.facebook.com/FleurLocaFlorist" target="_blank" class="btn btn-floating btn-lg"><i class="fab fa-facebook-f"></i></a>\
+                <!-- Instagram -->\
+                <a type="button" href="https://www.instagram.com/fleurlocaflorist" target="_blank" class="btn btn-floating btn-lg"><i class="fab fa-instagram"></i></a>\
+              </div>\
+          </div>\
+        </div>\
+      </div>\
+    </footer>\
+');
---
+++ javascript/header.js
@@ -0,0 +1,32 @@
+document.write('\
+<link rel="stylesheet" href="/css/header.css" />\
+<header>\
+      <nav class="navbar navbar-expand-lg">\
+        <div class="container-fluid">\
+          <a class="navbar-brand" href="mainpage.html">\
+          <img src="../images/fleurlogo.png" alt=""/></a>\
+          <button class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >\
+            <span class="toggler-icon top-bar"></span>\
+            <span class="toggler-icon middle-bar"></span>\
+            <span class="toggler-icon bottom-bar"></span>\
+          </button>\
+          <div class="collapse navbar-collapse" id="navbarNav">\
+            <ul class="navbar-nav mx-auto">\
+              <li class="nav-item">\
+                <a class="nav-link active" aria-current="page" href="mainpage.html">Home</a>\
+              </li>\
+              <li class="nav-item">\
+                <a class="nav-link" href="gallerypage.html">Gallery</a>\
+              </li>\
+              <li class="nav-item">\
+                <a class="nav-link" href="#">About</a>\
+              </li>\
+              <li class="nav-item">\
+                <a class="nav-link" href="#">Contact</a>\
+              </li>\
+            </ul>\
+          </div>\
+        </div>\
+      </nav>\
+      </header>\
+');
