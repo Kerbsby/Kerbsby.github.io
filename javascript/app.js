020150f9c2fd0da6d806e59d2d493f930287dc89
Kervy Yu <kerbs.yu@gmail.com>
2023-10-16 18:18:28 +0800

    recent overall changes

--- javascript/app.js
+++ javascript/app.js
@@ -13,3 +13,16 @@ function reveal(){
         }
     }
 }
+
+
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 500) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+}) 
